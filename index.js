df3584a2fd03d3dc2b5bd6cbc512369f568b37b6
Abhishek <abhishek.p@zapserver.com>
2022-09-29 15:54:29 +0530

    VCF File

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 const axios = require('axios');
 var app = express();
-const bodyParser = require('body-parser');
-var fs = require("fs");
-let vCardsJS = require('vcards-js');
-const FormData = require('form-data');
-require('dotenv').config();
+const bodyParser = require('body-parser');//bodyparser to json
+var fs = require("fs"); //store, access, read, write, rename files
+let vCardsJS = require('vcards-js');//vCards to import contacts into Outlook, iOS, Mac OS, and Android devices from your website or application
+const FormData = require('form-data'); //Package to create readable "multipart/form-data" streams.
+require('dotenv').config(); //Process.env will store all the process files
 const { v4: uuidv4 } = require('uuid');
-let vCard = vCardsJS();// This is your vCard instance, that
-// represents a single contact file
+let vCard = vCardsJS();// This is your vCard instance, that represents a single contact file
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
     res.send('Hello')
 })
@@ -22,9 +22,12 @@ vCard.middleName = params.middleName;
 let fileName = `${uuidv4()}.vcf`;
 let upload = vCard.saveToFile(fileName); // Save contact to VCF file
 const form = new FormData();
+
+// Reading Uploadcare Public key
 form.append('UPLOADCARE_PUB_KEY', process.env.UPLOADCARE_PUB_KEY);
 form.append('file', fs.readFileSync(fileName), fileName);
 
+// This Code is to upload the file from Local to Uploadcare File Server
 const response = axios.post(
 `${process.env.UPLOADCARE_URL}`,form,{
 headers: {
@@ -38,10 +41,11 @@ response.then(data => {
     })
 })
 
+//Port Adderess: 5000
 var server = app.listen(5000, function () {
 var host = server.address().address
 var port = server.address().port
-console.log("app listening at http://%s:%s", host, port)
+console.log("app listening", host, port)
 })
 
 
