4c368f9059b3310091398a177890eb2561abd9ed
Abhishek <abhishek.p@zapserver.com>
2022-10-04 14:07:32 +0530

    added photo and url in vcard

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ const FormData = require('form-data'); //Package to create readable "multipart/f
 require('dotenv').config(); //Process.env will store all the process files
 const { v4: uuidv4 } = require('uuid'); //Universally Unique IDentifier
 let vCard = vCardsJS();// This is your vCard instance, that represents a single contact file
-
+let resp = '';
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
-
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers",  req.header( 'access-control-request-headers' ));
+  res.setHeader('content-type', 'application/json');
+  req.header( "Accept","application/json");
+  next();
+});
 //Get request on root 
 app.get('/', function (req, res) {
     res.send('Hello')
@@ -46,105 +52,133 @@ EditRecordmbizcard_url=https%3A%2F%2Fckvaughan.com&
 EditRecordpicture_url=https%3A%2F%2Fstorage.mobilebuilder.net%2Fusers%2Fimages%2F8923c904-b895-4d2c-bc50-9b855b3478fa.jpg
 
 */
-// app.all('*', (req, res) =>{
-//     res.header( "Access-Control-Allow-Origin", "*" );
-//     res.header( "Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE" );
-//     res.header( "Access-Control-Allow-Headers", req.header( 'access-control-request-headers' ) );
-//     res.setHeader('content-type', 'text/xml');
-//     req.header( "Accept","application/xml");
-//     res.setHeader('content-type', 'application/json');
-//     req.header( "Accept","application/json");
-//     // Receive Access Token 
-//     // getAccessToken(res); 
-//   })
   
 //Axios POST object request with Parameters
-
-app.post('/update', function (req, res) {
-res.header( "Access-Control-Allow-Origin", "*" );
-res.header( "Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE" );
-res.header( "Access-Control-Allow-Headers", req.header( 'access-control-request-headers' ) );
-res.setHeader('content-type', 'text/xml');
-req.header( "Accept","application/xml");
-res.setHeader('content-type', 'application/json');
-req.header( "Accept","application/json");
-let params = req.body;
-let u_id = params.id; // PK
-vCard.firstName = params.EditRecordfirstName;
-vCard.lastName = params.EditRecordlastName;
-vCard.email = params.EditRecordemail_primary;
-vCard.homePhone = params.EditRecordphone_cell;
-let fileName = `${uuidv4()}.vcf`; // Saving filename to Universally Unique IDentifier
-let upload = vCard.saveToFile(fileName); // Save contact to VCF file
-const form = new FormData();
-
-// Reading Uploadcare Public key
-form.append('UPLOADCARE_PUB_KEY', process.env.UPLOADCARE_PUB_KEY);
-form.append('file', fs.readFileSync(fileName), fileName);
-
-// This Code is to upload the file from Local to Uploadcare File Server
-const response = axios.post(
-`${process.env.UPLOADCARE_URL}`,form,{
-headers: {
-...form.getHeaders()
-}
-});
-response.then(data => {
-    // res.send({"mbizcard_url" : `${process.env.UPLOADCARE_PATH+data.data.file}/`});
-    let filePath  = {mbizcard_url : `${process.env.UPLOADCARE_PATH+data.data.file}/`};
-    /* TODO: integrate Caspio API to update the table 
-      https://demo.caspio.com/rest/swagger#/Tables/TablesV2_UpdateRecords 
-      tablename: MBizCard_Users
-      
-      */
-      
-getAccessToken().then(data => {
-    // let url2 = "https://c7esh782.caspio.com/rest/v2/tables/MBizCard_Users/records?q.where=user_id="+user_id;
-    // let url2 = 'https://c7esh782.caspio.com/rest/v1/tables/MBizCard_Users/rows?q={"where":"user_id=UO0J3Y9N4"}';
-    let url2 = `https://c7esh782.caspio.com/rest/v2/tables/MBizCard_Users/records?q.where=user_id='${u_id}'`;
-    console.log(data)
-          try {
-              const resp2 = axios.put(url2,
-              filePath,
-              {
-                headers:{
-                'accept': 'application/json',
-                'Authorization': "Bearer " + data
-                }
-              });
-              resp2.then(caspData =>{
-               res.send({"success" : "data updated successfully", "status" : caspData.status})
-              }).catch(err =>{
-                res.error({"error" : "something went wrong!"})
-              })
-          } catch (err) {
-              res.error({"success" : "something went wrong!"})
-          }
-      }).catch(err =>{
-        res.error({"success" : "something went wrong!"})
-      })
-      
-    }, (error) => {
-    res.error(error.message);
-    })
+app.post('/update', async function (req, res) {
+  let params = req.body;
+  
+  let getVcf = await generateVcf(params);
+  if(getVcf.code == 200){
+    let uploadCareResp = await uploadVcftoUploadCare(getVcf.filename);
+    if(uploadCareResp.code == 200){
+      let vcardPath = await updateVcardPath(uploadCareResp.data,params.id);
+      if(vcardPath.code == 200){
+       res.send({"success" : "Data updated successfully!"}) 
+      }
+      else{
+        res.send({"error" : "something went wrong!"})
+      }
+    }
+    else{
+      res.send({"error" : "something went wrong!"})
+    }
+  }
+  else{
+    res.send({"error" : "something went wrong!"})
+  }
 })
 
 
 
 
 
-const getAccessToken = async () => {
-  let urly = 'https://c7esh782.caspio.com/oauth/token';
+const getAccessToken = async () => { 
     try {
-        const resp = await axios.post(urly, 'grant_type=client_credentials&client_id=327493aacca44205b3d8ab4e557d05b72bf0a41d988e843456&client_secret=c92ec092f92141e2a67be3ac65727675770b3411cb19b0ba4e');
-        let myAccessToken = resp.data.access_token; // Global variable ??
-        return myAccessToken;  
+        const response = await axios.post(`${process.env.CASPIO_AUTHTOKEN_PATH}`, 'grant_type=client_credentials&client_id=327493aacca44205b3d8ab4e557d05b72bf0a41d988e843456&client_secret=c92ec092f92141e2a67be3ac65727675770b3411cb19b0ba4e');
+        let myAccessToken = response.data.access_token; // Global variable ??
+        resp = {"code" : 200, "access_token" : myAccessToken}
     } catch (err) {
-        return err;
-        // console.error(err);
+        resp = {"code" : 400, "error" : "something went wrong!!"}
     }
+    return resp;
 };
+const uploadVcftoUploadCare = async (fileName) =>{
+  try{
+    const form = new FormData();
 
+    // Reading Uploadcare Public key
+    form.append('UPLOADCARE_PUB_KEY', process.env.UPLOADCARE_PUB_KEY);
+    form.append('file', fs.readFileSync(fileName), fileName);
+// console.log('2',`${process.env.UPLOADCARE_URL}`)
+    // This Code is to upload the file from Local to Uploadcare File Server
+    const response = await axios.post(
+    `${process.env.UPLOADCARE_URL}`,form,{
+    headers: {
+    ...form.getHeaders()
+    }
+    });
+    let succ = {"code" : response.status, "data" : response.data.file};
+    return succ;
+  }
+  catch(err){
+    return err;
+  }
+
+
+    
+}
+const generateVcf = async (params) => {
+  let vCard = vCardsJS();
+  try{
+  
+  let u_id = params.id;
+  // Set contact properties
+  vCard.firstName = params.EditRecordfirstName;
+  vCard.lastName = params.EditRecordlastName;
+  vCard.email = params.EditRecordemail_primary;
+  vCard.homePhone = params.EditRecordphone_cell;
+  vCard.url = params.EditRecordmbizcard_url;
+  // Add a profile photo by fetching from a URL
+
+  let url = params.EditRecordpicture_url;
+  let image = await axios.get(url,{responseType: 'arraybuffer'});
+  // Convert image to base 64           
+  let imageBase64 = Buffer.from(image.data).toString('base64');
+
+  // Set vCard photo to the base 64 value of the image
+  vCard.photo.embedFromString(imageBase64, 'image/jpeg');
+  let fileName = `${uuidv4()}.vcf`;
+  // Save contact to VCF file
+  vCard.saveToFile(fileName); 
+  resp = {"code" : 200, "filename" : fileName}
+   // Saving filename to Universally Unique IDentifier
+  // return fileName;
+  }
+  catch(err){
+  resp = {"code" : 400, "error" : "something went wrong!!"}
+  }
+  return resp;
+}
+
+const updateVcardPath = async (file,u_id) =>{
+  // console.log(file,u_id);
+  let url2 = `${process.env.CASPIO_MBIZCARD_TABLE_PATH}?q.where=user_id='${u_id}'`;
+
+  let filePath  = {"vcard" : `${process.env.UPLOADCARE_PATH+file}/`};
+   
+  let accessToken = await getAccessToken();
+  // console.log("--",accessToken); 
+  if(accessToken.code == 200){
+          try {
+              const resp2 = await axios.put(url2,
+              filePath,
+              {
+                headers:{
+                'accept': 'application/json',
+                'Authorization': "Bearer " + accessToken.access_token
+                }
+              });
+               resp = {"code" : resp2.status};
+          } catch (err) {
+            resp = {"code" : 400}
+          }
+         
+        }
+        else{
+          resp = {"code" : 401}
+        }
+         return resp;
+}
 //Port Adderess: 5000
 var server = app.listen(process.env.PORT || 5000, function () {
 var host = server.address().address
