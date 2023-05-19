56e4baabaf0beac5699f69334a091403a7a2109d
prakash_zaptech <prakash@zaptechsolutions.com>
2023-05-19 17:03:22 +0530

    stripe

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/processpayment", async (req, res ) => {
           Authorization: "Bearer " + accessToken.access_token,
         },
       });
-      const SECRET_KEY = resp2.data.Result[0].Secret;
+      const SECRET_KEY = resp2.data.Result[0].Test_Key;
       const stripe = require('stripe')(SECRET_KEY);
       let params = req.body;
       // console.log(params); return false;
@@ -88,7 +88,7 @@ app.post("/singlepayment", async (req, res) => {
       Authorization: "Bearer " + accessToken.access_token,
     },
   });
-  const SECRET_KEY = resp2.data.Result[0].Secret;
+  const SECRET_KEY = resp2.data.Result[0].Test_Key;
   const stripe = require('stripe')(SECRET_KEY);
   let exp_date = params.card_exp_date;
   params.card_exp_month = moment(exp_date).format("MM");
