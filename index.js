ba2f437f6692abef5f654dd9fdfe5bf35cf34f89
prakash_zaptech <prakash@zaptechsolutions.com>
2023-05-23 20:28:56 +0530

    stripe changes

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post("/processpayment", async (req, res ) => {
       const stripe = require('stripe')(SECRET_KEY);
       let params = req.body;
       // console.log(params); return false;
-      let exp_date = params.card_exp_date.split('/');
+      let exp_date = params.expiry_date.split('/');
       params.card_exp_month = exp_date[0]; //moment(exp_date).format("MM");
       params.card_exp_year = exp_date[1]; //moment(exp_date).format("YYYY");
     
